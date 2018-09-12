a2acbfe62e972ba9d0e834a00d01759ba36d9c65
The Owl <xeroxyde@gmail.com>
2018-09-12 12:20:33 -0400

    Creating API module.

---
+++ mangaeden/api.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+""" Mangaeden API Module """
+
+MANGA_LIST = 'https://www.mangaeden.com/api/list/{}/'
+MANGA = 'https://www.mangaeden.com/api/manga/{}/'
+CHAPTER = 'https://www.mangaeden.com/api/chapter/{}/'
+IMAGE = 'https://cdn.mangaeden.com/mangasimg/{}'
