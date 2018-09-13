4946cf40bf61ead5d84672c58939588c9824ca68
Manoah Bernier <manoahbernier@gmail.com>
2018-09-13 17:10:31 -0400

    Stripping useless features from api.get_dataset.

--- mangaeden/api.py
+++ mangaeden/api.py
@@ -6,26 +6,17 @@ import mangaeden.manga as manga
 import mangaeden.chapter as chapter
 import mangaeden.utils as utils
 
+LANG = {"EN": 0, "IT": 1}
 MANGA_LIST = 'https://www.mangaeden.com/api/list/{}/'
 MANGA = 'https://www.mangaeden.com/api/manga/{}/'
 CHAPTER = 'https://www.mangaeden.com/api/chapter/{}/'
 IMAGE = 'https://cdn.mangaeden.com/mangasimg/{}'
 
 
-def get_dataset(lang: int=0, page_n: int=None, n_mangas_per_page: int=None) -> dict:
-    """
-    Make a call to the API to get all mangas from the database.
-    Where lang can be 0 (English) or 1 (Italian)
-    """
+def get_dataset(lang: int) -> dict:
     params = dict()
-    url = MANGA_LIST.format(str(lang), page_n)
-
-    if page_n:
-        params['p'] = page_n
-    if n_mangas_per_page:
-        params['l'] = n_mangas_per_page
-
-    return utils.get_json(url, params)
+    url = MANGA_LIST.format(str(lang))
+    return utils.get_json(url, params)['manga']
 
 def get_manga(_id: str) -> manga.Manga:
     """Returns a manga data row."""
