954c24850d077f7fb705c9a7f3c98ff9c2590056
Manoah Bernier <manoahbernier@gmail.com>
2018-09-13 16:31:07 -0400

    Adding functions to handle image download.

--- mangaeden/api.py
+++ mangaeden/api.py
@@ -34,3 +34,15 @@ def get_manga(_id: str) -> manga.Manga:
 def get_chapter(_id: str) -> chapter.Chapter:
     """Returns a chapter data row."""
     return utils.get_json(CHAPTER.format(_id))
+
+def get_image(url: str) -> tuple:
+    """Returns an image data."""
+    req = utils.secure_get(IMAGE.format(url))
+    return tuple([req.content, req.headers.get('content-type')])
+
+def download_image(img: tuple, name: str, path: str=''):
+    f_name = name + "." + img[1].split('/')[1] # name and file type concatenation
+    f_path = f_name + path
+
+    with open(f_path, 'wb') as file:
+        file.write(img[0]) # writing image data to file
