6c69861dd2c5305e788fe904c4e0390c34ebb78e
Manoah Bernier <manoahbernier@gmail.com>
2018-09-14 08:53:25 -0400

    Adding functions to extract data from chapter rows (mangaeden.chapter).

--- mangaeden/chapter.py
+++ mangaeden/chapter.py
@@ -5,3 +5,25 @@
 import typing
 
 Chapter = typing.NewType('Chapter', dict)
+
+
+def get_images(chapter: Chapter) -> list:
+    """Gets chapter images."""
+    return chapter['images']
+
+def get_image_page(chapter: Chapter, row_n: int) -> int:
+    """Gets image page number."""
+    return chapter['images'][row_n][0]
+
+def get_image_url(chapter: Chapter, row_n: int) -> str:
+    """Gets image url."""
+    return chapter['images'][row_n][1]
+
+def get_image_resolution(chapter: Chapter, row_n: int) -> list:
+    """Gets image resolution."""
+    return list(
+        [
+            chapter['images'][row_n][2],
+            chapter['images'][row_n][3]
+        ]
+    )
